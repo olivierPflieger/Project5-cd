6eb648fcdd6f9cd586212f0e68be74d406b858fe
olivier Pflieger <keryss@gmail.com>
2026-05-25 13:06:13 +0200

    ci: Add playbook

--- main.tf
+++ main.tf
@@ -1,25 +1,44 @@
 provider "aws" {
-  region = "eu-central-1"
+  region = "us-east-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_instance" "oc_project5" {
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-04a81a99f5ec58529"
   instance_type = "t3.micro"
 
-  key_name = "oc-key-eu-central-1"
-
   tags = {
-    Name = "learn-terraform"
+    Name = "OpenClassrooms-EDO-P5"
+  }
+
+  vpc_security_group_ids = ["${aws_security_group.oc_project5_sg.id}"]
+  key_name               = "oc-project5-key-us-east-1"
+}
+
+resource "aws_security_group" "oc_project5_sg" {
+  name = "allow-ssh"
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
